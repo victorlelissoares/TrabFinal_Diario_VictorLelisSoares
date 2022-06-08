9ffd41ef91e19b35610abd6c0fe31e3f78277c7f
victorlelissoares <victorlelis2011@gmail.com>
2022-06-08 08:35:36 -0400

    Initial commit

---
+++ app/src/main/java/com/example/trabfinal_diario_victorlelissoares/RegisterFragment.java
@@ -0,0 +1,47 @@
+package com.example.trabfinal_diario_victorlelissoares;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.trabfinal_diario_victorlelissoares.databinding.FragmentRegisterBinding;
+
+public class RegisterFragment extends Fragment {
+
+    private FragmentRegisterBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentRegisterBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(RegisterFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
