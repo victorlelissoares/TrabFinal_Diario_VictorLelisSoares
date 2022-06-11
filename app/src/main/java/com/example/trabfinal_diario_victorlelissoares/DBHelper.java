9b240645f5938f927eecc4405959971ec358a871
victorlelissoares <victorlelis2011@gmail.com>
2022-06-10 20:36:39 -0400

    implement class user

---
+++ app/src/main/java/com/example/trabfinal_diario_victorlelissoares/DBHelper.java
@@ -0,0 +1,27 @@
+package com.example.trabfinal_diario_victorlelissoares;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 2;
+    private static final String DATABASE_NAME = "Diario.db";
+
+    SQLiteDatabase db;
+    public DBHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/example/trabfinal_diario_victorlelissoares/User.java
@@ -0,0 +1,50 @@
+package com.example.trabfinal_diario_victorlelissoares;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private int idUser;
+    private String nome;
+    private String email;
+    private String senha;//substituir futuramente pelo hash da senha ao inves da própria
+    //fora a foto do usuario
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "nome: " + nome + '\'' +
+                ", email: " + email + '\'' +
+                '}';
+    }
+}
